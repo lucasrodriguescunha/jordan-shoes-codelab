19afdedbb292249b63520618bc602fd0366f898c
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-08-15 13:42:56 -0300

    feat: add SectionTitle component to Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header/Header';
+import SectionTitle from '@/components/SectionTitle/SectionTitle';
 
 export default function Page() {
     return (
         <>
             <Header/>
+            <SectionTitle/>
         </>
     );
 }
