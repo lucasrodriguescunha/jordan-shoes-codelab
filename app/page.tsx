6cbc17dc5fa0629c7a273d88ba36533495c66a1f
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-08-16 11:51:25 -0300

    feat: add ProductGrid component to Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/Header/Header';
 import SectionTitle from '@/components/SectionTitle/SectionTitle';
+import ProductGrid from '@/components/ProductGrid/ProductGrid';
 
 export default function Page() {
     return (
         <>
             <Header/>
             <SectionTitle/>
+            <ProductGrid/>
         </>
     );
 }
