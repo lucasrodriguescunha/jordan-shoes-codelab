9c28fd755fe9cdbb842a52c82e678167c3baebdb
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-08-19 13:26:23 -0300

    feat: enhance Jest configuration with coverage settings and new test command

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,24 @@ const customJestConfig = {
         '^@/pages/(.*)$': '<rootDir>/pages/$1',
     },
     testEnvironment: 'jest-environment-jsdom',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'components/**/*.{js,jsx,ts,tsx}',
+        'app/**/*.{js,jsx,ts,tsx}',
+        '!components/**/*.module.css',
+        '!**/*.d.ts',
+        '!**/node_modules/**',
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'html'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
